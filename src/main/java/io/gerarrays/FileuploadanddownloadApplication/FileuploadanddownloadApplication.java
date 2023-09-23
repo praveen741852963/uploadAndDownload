ff6566c9bd5dd976f0f1bc777a3635aa1a5fc7e4
Praveen Yadav <yadavpraveen466@gmail.com>
2023-09-23 13:50:17 +0530

    Initial setup

---
+++ src/main/java/io/gerarrays/FileuploadanddownloadApplication/FileuploadanddownloadApplication.java
@@ -0,0 +1,14 @@
+package io.gerarrays.FileuploadanddownloadApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadanddownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadanddownloadApplication.class, args);
+		System.out.println("version 1");
+	}
+
+}
---
+++ src/test/java/io/gerarrays/FileuploadanddownloadApplication/FileuploadanddownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gerarrays.FileuploadanddownloadApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadanddownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
