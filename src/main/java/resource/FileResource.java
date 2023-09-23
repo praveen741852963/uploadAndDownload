3d04d84a9056a56c9ed6bdd917d1b40ba8d24f58
Praveen Yadav <yadavpraveen466@gmail.com>
2023-09-23 18:50:08 +0530

    feat: Add resource package with FileResource class

---
+++ src/main/java/resource/FileResource.java
@@ -0,0 +1,18 @@
+package resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
+public class FileResource {
+
+    //define a location
+
+
+    //Define a method to upload files
+
+    //Define a method to download files
+
+
+}
